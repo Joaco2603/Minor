5b82777610efd77a3a553d9cab8e50b1627b4a4d
Joaco2603 <111933983+Joaco2603@users.noreply.github.com>
2023-04-17 21:42:28 -0600

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');  //Se llama a mongoose
 
-mongoose.connect('mongodb+srv://Minor_Daniel:DanielCanelo1@cluster0.raouysj.mongodb.net/test', {  //Se conecta a mi base de datos 
+mongoose.connect('', {  //Se conecta a mi base de datos 
     useNewUrlParser: true
 })
     .then(db => console.log('Conectado papi'))//Se conecto de forma correcta
